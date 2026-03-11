96bff6bb2ee143b7e4a1e22a60b1a396eb997d0e
kunal rai <kunalatstudy@gmail.com>
2026-03-11 21:00:49 +0530

    solved day4 rock paper sicisior game

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,6 @@
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+import random
+
+random_no = random.randint(0, len(friends)-1)
+
+print(friends[random_no])
---
+++ Day 4/Random Module/task.py
@@ -0,0 +1,10 @@
+#heads and tails
+
+import random
+
+rand_int = random.randint(0, 1)
+
+if rand_int == 1:
+    print("Heads")
+else:
+    print("Tails")

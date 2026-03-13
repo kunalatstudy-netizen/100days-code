835cf66df5e5f137d7b0d928d3e73a36a27047ba
kunal rai <kunalatstudy@gmail.com>
2026-03-13 10:21:44 +0530

    completed day 6

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,12 @@
+# def my_function():
+#     print("Hello World")
+#
+#
+# print(my_function())
+
+def get_user_name():
+    name = input("What is your name?")
+    print("Hello, " + name)
+
+print("Hello")
+get_user_name()

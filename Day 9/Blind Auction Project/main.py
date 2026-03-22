77d82ced3c9f8cf79725c05f1672d43b700af276
kunal rai <kunalatstudy@gmail.com>
2026-03-22 19:31:18 +0530

    solved the basic of the auction project

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,29 @@
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
+from art import logo
+print(logo)
 
+biders = {}
+game_over = False
+def winner(bider):
+    winner_win = 0
+    for key in biders:
+        if bid > winner_win:
+            winner_win = bid
+        else:
+            winner_win = winner_win
+    print(f"The winner is {winner_win} with a bid of ${bid}")
+
+
+
+
+
+while game_over == False:
+    name = input("What is your name? ")
+    bid = int(input("What is your bid? "))
+    more_bider = input("Are there any other bidders? Type 'yes' or 'no': ")
+    biders[name] = bid
+    if more_bider == "yes":
+        game_over = False
+    else:
+        game_over = True
+        winner(biders[name])
 

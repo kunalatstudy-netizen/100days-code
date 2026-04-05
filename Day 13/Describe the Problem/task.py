2373bbbf6d3fd4622fe30f765ba1c4546f53aa80
kunal rai <kunalatstudy@gmail.com>
2026-04-05 11:23:06 +0530

    solved day 13 and learned debuging skill

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -1,12 +1,16 @@
 def my_function():
-    for i in range(1, 20):
+    for i in range(1, 21):
         if i == 20:
             print("You got it")
 
 
-my_function()
+a = my_function()
+print(a)
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
+#for loop is looping for 1 to 20 times and wher i is equal to 20 then we should print you got it
 # 2. When is the function meant to print "You got it"?
+#when i is equal to 20
 # 3. What are your assumptions about the value of i?
+# it will not be 20 because it will stop on 19
